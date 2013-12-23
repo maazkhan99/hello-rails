436a90ace048f35dfa2eb4c505215ae40abfc57e
Maaz Khan <maaz.khan99@gmail.com>
2013-12-23 15:24:16 +0530

    Minor change rollback

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!, except: [:index]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
   # GET /pins
